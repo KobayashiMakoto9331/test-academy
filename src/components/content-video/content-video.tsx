249158a09fd8374b2ca970245ce3f8e042db047d
kobayashi28tc <kobayashi.makoto@d-publishing.jp>
2025-01-19 12:25:57 +0900

    Create test component

--- src/components/content-video/content-video.tsx
+++ src/components/content-video/content-video.tsx
@@ -1,5 +1,14 @@
+"use client";
+import { WistiaPlayer } from "@wistia/wistia-player-react";
 import React from "react";
 
 export const ContentVideo = () => {
-  return <div>content-video</div>;
+  return (
+    <WistiaPlayer
+      mediaId="vp8amj0ovf"
+      playerColor="1e64f0"
+      onPlay={() => console.log("Wistia video is playing!")}
+      aspect={16 / 9}
+    />
+  );
 };
